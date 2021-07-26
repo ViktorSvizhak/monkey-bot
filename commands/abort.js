945ece207dfe1621e58fcd75dc1f2b21c8844f31
viktorsvizhak <viktor.svizhak@gmail.com>
2021-07-26 16:30:30 +0300

    implement current command

--- commands/abort.js
+++ commands/abort.js
@@ -2,6 +2,8 @@ const musicPlayer = require('../modules/music/musicPlayer');
 
 module.exports = {
     commands: 'abort',
+    minArgs: 0,
+    maxArgs: 0,
     callback: (message, arguments) => {
         musicPlayer.abortConnection(message.guild.id);
     },
