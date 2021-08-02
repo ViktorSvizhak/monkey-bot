625886aaa9c09212d69abde3c655b51128b0824f
viktorsvizhak <viktor.svizhak@gmail.com>
2021-08-02 12:00:42 +0300

    queueEmbed improvements

--- modules/embed/queueEmbed.js
+++ modules/embed/queueEmbed.js
@@ -1,18 +1,22 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = {
-    createEmbed: (queueInfo) => {
-        var songList = '';
-        var index = 0;
-        queueInfo.queueSong.forEach(element => {
-            songList += `${++index}. ${element.title}\n`;
-        })
-        
-        return new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Queue of songs')
-            .setTimestamp()
-            .addField('Currently play',  queueInfo.currentSong.title, false)
-            .addField('Songs in queue', songList, false);
-    }
+module.exports = (queueInfo) => {
+    return {
+        embed: createEmbed(queueInfo)
+    };
+} 
+
+function createEmbed(queueInfo) {
+    var songList = '';
+    var index = 0;
+    queueInfo.queueSong.forEach(element => {
+        songList += `${++index}. ${element.title}\n`;
+    })
+    
+    return new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Queue of songs')
+        .setTimestamp()
+        .addField('Currently play',  queueInfo.currentSong.title, false)
+        .addField('Songs in queue', songList, false);
 }
